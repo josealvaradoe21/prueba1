0e18d50c67d7ab7a45382c9c28938facb26ab5e1
josealvaradoe21 <44307378+josealvaradoe21@users.noreply.github.com>
2020-10-08 22:23:59 -0500

    Comentarios

--- demo2/src/main/java/com/example/demo/Movie.java
+++ demo2/src/main/java/com/example/demo/Movie.java
@@ -6,7 +6,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
+//comentario
 @Entity
 @Table(name = "movies")
 public class Movie {
